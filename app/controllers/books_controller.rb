74b565d3ca007192d62f94c6be7cdd743325e091
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2022-09-21 16:22:15 +0100

    add link to each book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,14 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @copies = Copy.all
+  end
+
+  def add
+    puts 'Hey'
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 end
