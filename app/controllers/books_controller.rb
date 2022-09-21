b2091b83abdad8a1cdd2c08bad3957be47309b9c
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2022-09-21 16:50:22 +0100

    add new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,26 @@ class BooksController < ApplicationController
     @copies = Copy.all
   end
 
-  def add
-    puts 'Hey'
-  end
-
   def show
     @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :ISBN)
   end
 end
