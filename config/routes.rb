a00e5f2c13429a94ab4c976229290491308e37af
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2022-09-22 15:49:20 +0100

    user log in stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+    get "/users/sign_out", to: "devise/sessions#destroy"
+  end
+
+  devise_for :users
   get 'books/index'
 
   # get "books/:id", to: "books#show"
@@ -6,6 +13,8 @@ Rails.application.routes.draw do
   resources :books
   resources :copies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
 
   # Defines the root path route ("/")
   # root "articles#index"
